cae8e4b55fb23bf6e0c58cdde6eed511db6fffeb
andreinegara <andre.negara96@gmail.com>
2021-05-29 12:53:40 +0100

    Player Movement & Collisions with Obstacles

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed;
+
+	// Update is called once per frame
+	void Update () {
+		transform.position = transform.position + Vector3.left * speed * Time.deltaTime;
+		if (transform.position.x < -15){
+			Destroy (gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerJump.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJump : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public int jumpForce = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)){
+            rb = GetComponent<Rigidbody2D>();
+            rb.AddForce(Vector2.up * jumpForce , ForceMode2D.Impulse);
+        }
+    }
+}
