064233361669598cc249c561dfeb61029b23951d
andreinegara <andre.negara96@gmail.com>
2021-05-29 20:47:18 +0100

    Score
    Added a Text UI to show score after passing obstacles

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    private Text text;
+    private int score=0;
+
+    void Start()
+    {
+        score = 0;
+        text = GameObject.Find("Score").GetComponent<Text>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        //When PLayer collides with scoreArea
+        if (other.gameObject.name == "scoreArea")
+        {
+            score += 1;
+
+            //Changes Score text with the updated score
+            text.text = "Score: " + score;
+        }
+    }
+}
